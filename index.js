2f17a0c0b75945784cd45ad7d9e4538dd10eac06
Tucker Steil <tuckersteil@gmail.com>
2022-03-30 16:17:29 -0400

    commit tree 6ac51eb4eba212ce268557b7ad2f6ff4a8432a3d

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+function writeCards(names, event){
+    let messages = [];
+    for (let i = 0; i < names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+function countDown(x){
+    let count = x;
+    while (count >= 0){
+        console.log(count);
+        count--;
+    }
+}
